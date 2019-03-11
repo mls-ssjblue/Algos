e75c0d632aea80452d943610b585bd938d2d9e61
Susheel <mlsusheel@gmail.com>
2019-03-11 18:01:41 +0800

    Valid parenthesis mls

--- src/main/java/validparentheis/ValidParenthesis.java
+++ src/main/java/validparentheis/ValidParenthesis.java
@@ -4,52 +4,50 @@ import java.util.Stack;
 
 public class ValidParenthesis {
     public static void main(String[] args) {
-        String s = "()[]{}";
+        String s = "{{}}";
         System.out.println(isValid(s));
     }
 
     public static boolean isValid(String s) {
-        if(s.isEmpty()) return true;
-        final int STRING_LENGTH = s.length();
-        int lastOpen = 0;
-        Stack<Character> openBraces = new Stack();
-        int i =0;
-        while(i < STRING_LENGTH ){
-            if(isOpenBraces(s, i)){
-                openBraces.push(s.charAt(i));
+        if (s.isEmpty()) return true;
+        int i = 0;
+        Stack<Character> openBraceStack = new Stack<>();
+        while (i < s.length()) {
+            char currentChar = s.charAt(i);
+
+            if (isOpenBraces(s, i)) {
+                openBraceStack.push(currentChar);
                 i++;
-                continue;
+            } else if (openBraceStack.isEmpty()) return false;
+            else {
+                Character lastOpenBrace = openBraceStack.pop();
+                if (isComplement(currentChar, lastOpenBrace)) i++;
+                else return false;
             }
-            lastOpen = i -1;
-            break;
         }
-        int j = lastOpen + 1;
-        while(j < STRING_LENGTH){
-            if(openBraces.isEmpty()) return false;
-            Character lastOpenChar = openBraces.pop();
-            Character firstClose = s.charAt(j);
-            if(isComplement(firstClose, lastOpenChar)){
-                j ++;
-            }
-            else return false;
-        }
-        if(openBraces.isEmpty() != true) return false;
-        return true;
+        if(!openBraceStack.isEmpty()) return  false;
+        else return true;
     }
+
 
     private static boolean isComplement(char closed, Character open) {
         switch(open){
             case '{' : if(closed == '}'){
                 return true;
             }
+            break;
             case '[':  if(closed == ']'){
                 return true;
             }
+            break;
+
             case '(': if(closed == ')'){
                 return true;
             }
+
             default: return  false;
         }
+        return false;
     }
 
     private static boolean isOpenBraces(String s, int i) {
