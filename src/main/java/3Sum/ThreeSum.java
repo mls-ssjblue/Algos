2bfad77fd050ee1750da73e368445483e20e53ef
Susheel <mlsusheel@gmail.com>
2019-03-11 16:24:11 +0800

    Three sum not working

--- src/main/java/3Sum/ThreeSum.java
+++ src/main/java/3Sum/ThreeSum.java
@@ -1,40 +1,63 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class ThreeSum {
-    public static void main(String [] args){
-        int[] nums = new int[]{-1,0,1,2,-1,-4};
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
         List<List<Integer>> answers = threeSum(nums);
     }
 
-    public static List<List<Integer>> threeSum(int[] nums){
+    public static List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> answers = new ArrayList<>();
-        Map<Integer,List<Integer>> complementMap = getComplementMap(nums);
-        for(int i =0;i<nums.length;i++){
-            List<Integer> complements = complementMap.get(nums[i]*-1);
-            if(!complements.isEmpty()){
-                List<Integer> validResult = Arrays.asList( Integer.valueOf(nums[i]));
-                validResult.addAll(complements);
-                answers.add(validResult);f
+        Map<Integer, List<List<Integer>>> complementMap = getComplementMap(nums);
+        for (int i = 0; i < nums.length; i++) {
+            List<List<Integer>> complementCombos = complementMap.get(nums[i] * -1);
+            if (complementCombos != null) {
+                List<Integer> validResult = new ArrayList<>();
+                Integer e = Integer.valueOf(nums[i]);
+                validResult.add(e);
+                int finalI = i;
+                List<List<Integer>> validCombos = complementCombos.stream()
+                        .filter(combo -> combo.stream().filter(index -> index == finalI).collect(Collectors.toList()).size() == 0)
+                        .collect(Collectors.toList());
+                if (validCombos.size() > 0) {
+                    int finalI1 = i;
+                    List<List<Integer>> finalCombos = new ArrayList<>();
+                    validCombos.forEach(combo -> {
+                        List<Integer> temp = combo.stream()
+                                .map(index->Integer.valueOf(nums[index]))
+                                .collect(Collectors.toList());
+                       temp.add(nums[finalI1]);
+                       finalCombos.add(temp);
+                   });
+                    finalCombos.forEach(combo->answers.add(combo));
+
+                }
             }
         }
-        System.out.println(answers);
-        
-        return null;
+
+        List<List<Integer>> distinctAnswers = answers.stream()
+                .map(list-> list.stream().sorted().collect(Collectors.toList()))
+                .distinct().collect(Collectors.toList());
+        System.out.println(distinctAnswers);
+
+        return distinctAnswers;
     }
-   fvdfg
 
-    private static Map<Integer, List<Integer>> getComplementMap(int[] nums){
+    private static Map<Integer, List<List<Integer>>> getComplementMap(int[] nums) {
 
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        Map<Integer, List<List<Integer>>> a = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            for(int j=0; j<nums.length; j++){
-                map.put(Integer.valueOf(nums[i] + nums[j]), Arrays.asList(nums[i],nums[j]));
+        Map<Integer, List<List<Integer>>> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                List<List<Integer>> combos = Optional.ofNullable(map.get(nums[i] + nums[j]))
+                        .orElse(new ArrayList<>());
+                combos.add(Arrays.asList(i, j));
+                map.put(nums[i] + nums[j], combos);
             }
         }
-        System.out.println(map.toString());
+        System.out.println(map);
         return map;
-        
+
     }
 }
