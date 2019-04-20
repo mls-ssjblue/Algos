d2aa36775bab2a09f6341b6ab06041f70903c949
mlssjblue <mlsusheel@gmail.com>
2019-04-20 20:21:33 +0800

    DFS - Sorted Array to Sorted BST

---
+++ src/main/java/dfs/sortarr_to_sortbst/SortedArrayToSortedBST.java
@@ -0,0 +1,39 @@
+package dfs.sortarr_to_sortbst;
+
+import dfs.TreeNode;
+
+public class SortedArrayToSortedBST {
+    public static void main(String[] args) {
+        int[] arr = {-10, -3, 0, 5, 9};
+        printBT((sortedArrayToBST(arr, 0, arr.length-1)));
+    }
+
+    private static void printBT(TreeNode bt) {
+
+        System.out.println(bt.val);
+        printBT(bt.left);
+        printBT(bt.right);
+    }
+
+    public static TreeNode sortedArrayToBST(int[] arr, int start, int end) {
+
+        /* Base Case */
+        if (start > end) {
+            return null;
+        }
+
+        /* Get the middle element and make it root */
+        int mid = (start + end) / 2;
+        TreeNode node = new TreeNode(arr[mid]);
+
+        /* Recursively construct the left subtree and make it
+         left child of root */
+        node.left = sortedArrayToBST(arr, start, mid - 1);
+
+        /* Recursively construct the right subtree and make it
+         right child of root */
+        node.right = sortedArrayToBST(arr, mid + 1, end);
+
+        return node;
+    }
+}
