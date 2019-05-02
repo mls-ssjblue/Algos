54f60a7377bd649506fb413e4f6b043312e89485
mlssjblue <mlsusheel@gmail.com>
2019-05-02 23:40:22 +0800

    ctci arrays

---
+++ src/main/java/ctci/arrays/OneAway.java
@@ -0,0 +1,59 @@
+package ctci.arrays;
+
+import java.util.Scanner;
+
+public class OneAway {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String first = sc.next();
+        String second = sc.next();
+
+        System.out.println(isOneAway(first,second));
+    }
+
+    private static boolean isOneAway(String first, String second) {
+
+        if (first.equals(second)) return true;
+        int l1 = first.length();
+        int l2 = second.length();
+        int diff = 0;
+        if (Math.abs(l1 - l2) > 1) return false;
+        int max = Math.max(l1, l2);
+
+        if (l1 > l2) {
+            for (int i = 0, j = 0; i < max; i++) {
+                if(j==l2) return true;
+                if (first.charAt(i) == second.charAt(j)) {
+                    j++;
+                } else {
+                    diff++;
+                }
+            }
+            if (diff > 1) return false;
+            return true;
+        } else if (l1 < l2) {
+            for (int i = 0, j = 0; j < max; j++) {
+                if(i==l1) return true;
+                if (first.charAt(i) == second.charAt(j)) {
+                    i++;
+                } else {
+                    diff++;
+                }
+            }
+            if (diff > 1) return false;
+            return true;
+        } else {
+            for (int i = 0, j = 0; i < max; i++, j++) {
+                if (first.charAt(i) != second.charAt(i)) {
+                    diff++;
+                }
+
+            }
+            if (diff > 1) return false;
+            return true;
+        }
+
+    }
+}
---
+++ src/main/java/ctci/arrays/ZeroMatrix.java
@@ -0,0 +1,51 @@
+package ctci.arrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ZeroMatrix {
+    public static void main(String[] args) {
+        int[][] ip = new int[][]{
+                {1, 0, 3},
+                {1, 2, 0},
+                {1, 5, 3},
+        };
+
+         transform(ip);
+        for(int i = 0 ;i<3;i++){
+            System.out.println();
+            for(int j = 0;j<3;j++)
+                System.out.print(ip[i][j]);
+        }
+    }
+
+    private static void transform(int[][] ip) {
+        Set<Integer> zeroRows = new HashSet<>();
+        Set<Integer> zeroColumns = new HashSet<>();
+        int R = ip.length;
+        int C = ip[0].length;
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if(zeroColumns.contains(j) || zeroRows.contains(i)) continue;
+                if(ip[i][j] == 0){
+                    zeroRows.add(i);
+                    zeroColumns.add(j);
+                    setRowToZero(i, ip);
+                    setColToZero(j, ip);
+                }
+            }
+        }
+    }
+
+    private static void setColToZero(int col, int[][] ip) {
+        for(int i =0;i<ip.length;i++){
+            ip[i][col] = 0;
+        }
+    }
+
+    private static void setRowToZero(int row, int[][] ip) {
+        for(int i =0;i<ip[0].length;i++){
+            ip[row][i] = 0;
+        }
+    }
+}
