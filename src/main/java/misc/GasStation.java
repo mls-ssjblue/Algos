df796e6efb6bcd587bc4af0c6965b754cd8424f3
mlssjblue <mlsusheel@gmail.com>
2019-07-20 15:58:20 +0800

    Gas station optimized

--- src/main/java/misc/GasStation.java
+++ src/main/java/misc/GasStation.java
@@ -17,33 +17,24 @@ public class GasStation {
         Assertions.assertEquals(-1,
                 canCompleteCircuit(new int[]{3, 3, 4}, new int[]{3, 4, 4}));
         Assertions.assertEquals(4,
-                canCompleteCircuit(new int[]{5,1,2,3,4}, new int[]{4,4,1,5,1}));
+                canCompleteCircuit(new int[]{5, 1, 2, 3, 4}, new int[]{4, 4, 1, 5, 1}));
     }
 
     private int canCompleteCircuit(int[] gas, int[] cost) {
-        int startIndex = 0;
-        int tank = gas[0];
-        int N = gas.length;
-        int distance = 0;
-        List<Integer> startIndicesUsed = new ArrayList<>();
-        for(int i = 0; i < N; i ++ ) {
-            if(distance == N) return startIndex;
-            if(tank < cost[i]){
-                if(startIndicesUsed.size() == N || startIndex==N-1) return -1;
-                startIndex++;
-                i= startIndex-1;
-                distance = 0;
-                startIndicesUsed.add(startIndex);
-                tank = gas[startIndex];
-            }
-            else{
-                tank += (i!=N-1) ? gas[i + 1] - cost[i]: gas[0] - cost[N-1];
-                if(i == N-1) i = -1;
-                distance ++;
-            }
+        int start = 0;
+        int total = 0;
+        int tank = 0;
 
+        for (int i = 0; i < gas.length; i++) {
+            tank = tank + gas[i] - cost[i];
+            if (tank < 0) {
+                start = i + 1;
+                total += tank;
+                tank = 0;
+            }
         }
-        return startIndex;
+
+        return (total + tank < 0) ? -1 : start;
 
     }
 }
